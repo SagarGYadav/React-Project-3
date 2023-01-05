7fe7e5594f8cc7311a2824e3d0b15821ec070815
SagarGYadav <118979610+SagarGYadav@users.noreply.github.com>
2023-01-05 17:41:38 +0530

    Working with 'ref's

--- src/components/Helpers/Wrapper.js
+++ src/components/Helpers/Wrapper.js
@@ -3,3 +3,7 @@ const Wrapper = (props) => {
 };
 
 export default Wrapper;
+
+// setEnteredAge useState here it's became string not a number.
+//  to convert it into number (or we want to be super safe)
+// we added '+' before enteredAge . so now it will give positive number.
